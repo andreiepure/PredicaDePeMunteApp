4372c3afb9aba56409e349ae971f1eb97cef9f39
Andrei Epure <andrei.epure@cti.pub.ro>
2015-11-27 23:39:35 +0000

    added the text for the 3 chapters
    - added the text for the 3 chapters of the Sermon on the Mount
    - added the script to get the sermon
    - added Bootstrap modal for chapter 5
    - in Matei5_Initial I saved the initial layout before making the script
    (should remove)
    - added jquery file needed for modal
    Thanks God!

---
+++ PredicaDePeMunte/scripts/matei5.js
@@ -0,0 +1,38 @@
+﻿
+(function () {
+    $(document).ready(function () {
+        $("#btn-5-3").click(function () {
+            $("#modal-5-3").modal();
+        });
+        $("#btn-5-11").click(function () {
+            $("#modal-5-11").modal();
+        });
+        $("#btn-5-15").click(function () {
+            $("#modal-5-15").modal();
+        });
+        $("#btn-5-17").click(function () {
+            $("#modal-5-17").modal();
+        });
+        $("#btn-5-18").click(function () {
+            $("#modal-5-18").modal();
+        });
+        $("#btn-5-22").click(function () {
+            $("#modal-5-22").modal();
+        });
+        $("#btn-5-29").click(function () {
+            $("#modal-5-29").modal();
+        });
+        $("#btn-5-31").click(function () {
+            $("#modal-5-31").modal();
+        });
+        $("#btn-5-37").click(function () {
+            $("#modal-5-37").modal();
+        });
+        $("#btn-5-39").click(function () {
+            $("#modal-5-39").modal();
+        });
+        $("#btn-5-46").click(function () {
+            $("#modal-5-46").modal();
+        });
+    });
+} )();
