faf6fb7914b151ed8a6563b2862fd78aa47c6388
Andrei Epure <andrei.epure@cti.pub.ro>
2015-12-29 23:59:34 +0000

    Added font size setting
    - added font size setting, with local storage (added modernizr library
    and settings page)
    - changed the buttons everywhere, including inside  the modals
    - adding images for splash images (tested on win phone and old android)

--- PredicaDePeMunte/scripts/matei7.js
+++ PredicaDePeMunte/scripts/matei7.js
@@ -1,5 +1,27 @@
 ﻿(function () {
     $(document).ready(function () {
+
+        if (Modernizr.localstorage) {
+            var foo = localStorage.getItem('fontSize');
+            console.log(foo);
+            if (foo != null)
+            {
+                var myElements = document.querySelectorAll('li');
+                for (var i = 0; i < myElements.length; i++) {
+                    myElements[i].style.fontSize = foo;
+                }
+                var myElements = document.querySelectorAll('.modal-body');
+                for (var i = 0; i < myElements.length; i++) {
+                    myElements[i].style.fontSize = foo;
+                }
+
+
+            }
+        } else {
+            console.log("local storage not available");
+        }
+
+
         $("#btn-7-1").click(function () {
             $("#modal-7-1").modal();
         });
---
+++ PredicaDePeMunte/scripts/settings.js
@@ -0,0 +1,27 @@
+﻿
+(function () {
+
+    $(document).ready(function () {
+
+        if (Modernizr.localstorage) {
+            var foo = localStorage.getItem('fontSize');
+            console.log(foo);
+            if (foo != null)
+            {
+                $('select option[value="' + foo + '"]').attr("selected", true);
+            }
+        } else {
+            console.log("local storage not available");
+        }
+
+        $('#font-size-selector').change(function () {
+            console.log("Will set fontSize to " + this.value);
+            if (Modernizr.localstorage) {
+                 foo = localStorage.setItem('fontSize', this.value);
+            }
+            else {
+                console.log("local storage not available");
+            }
+        });
+    });
+} )();
